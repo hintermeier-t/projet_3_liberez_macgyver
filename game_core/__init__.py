53a870170c80b31703eb9d8868ee563603c92c63
Thomas Hintermeier <thomas.hintermeier@outlook.fr>
2020-01-29 11:48:21 +0100

    Nettoyage de la branche

--- game_core/__init__.py
+++
@@ -1,9 +0,0 @@
-#-*coding: UTF-8-*
-#!/usr/bin/python3.8
-
-from .board import GamingBoard
-from .guard import EnnemyGuard
-from .items import UseableItem
-from .hero import McGyver
-from .position import Position
-from .game import Game
